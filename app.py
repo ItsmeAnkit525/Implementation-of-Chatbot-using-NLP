e102f5937232f4d04dd330039054cd60036bc5ab
Ankit <ankitagrawalhero@gmail.com>
2024-12-17 01:06:00 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,124 @@
+import os
+import json
+import datetime
+import csv
+import nltk
+import ssl
+import streamlit as st
+import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+ssl._create_default_https_context = ssl._create_unverified_context
+nltk.data.path.append(os.path.abspath("nltk_data"))
+nltk.download('punkt')
+
+# Load intents from the JSON file
+file_path = os.path.abspath("./intents.json")
+with open(file_path, "r") as file:
+    intents = json.load(file)
+
+# Create the vectorizer and classifier
+vectorizer = TfidfVectorizer(ngram_range=(1, 4))
+clf = LogisticRegression(random_state=0, max_iter=10000)
+
+# Preprocess the data
+tags = []
+patterns = []
+for intent in intents:
+    for pattern in intent['patterns']:
+        tags.append(intent['tag'])
+        patterns.append(pattern)
+
+# training the model
+x = vectorizer.fit_transform(patterns)
+y = tags
+clf.fit(x, y)
+
+def chatbot(input_text):
+    input_text = vectorizer.transform([input_text])
+    tag = clf.predict(input_text)[0]
+    for intent in intents:
+        if intent['tag'] == tag:
+            response = random.choice(intent['responses'])
+            return response
+
+counter = 0
+
+def main():
+    global counter
+    st.title("Customer Support Chatbot")
+
+    # Create a sidebar menu with options
+    menu = ["Home", "Conversation History", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    # Home Menu
+    if choice == "Home":
+        st.write("Welcome to our customer support chatbot. How can I assist you today?")
+
+        # Check if the chat_log.csv file exists, and if not, create it with column names
+        if not os.path.exists('chat_log.csv'):
+            with open('chat_log.csv', 'w', newline='', encoding='utf-8') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow(['User Input', 'Chatbot Response', 'Timestamp'])
+
+        counter += 1
+        user_input = st.text_input("You:", key=f"user_input_{counter}")
+
+        if user_input:
+
+            # Convert the user input to a string
+            user_input_str = str(user_input)
+
+            response = chatbot(user_input)
+            st.text_area("Chatbot:", value=response, height=120, max_chars=None, key=f"chatbot_response_{counter}")
+
+            # Get the current timestamp
+            timestamp = datetime.datetime.now().strftime(f"%Y-%m-%d %H:%M:%S")
+
+            # Save the user input and chatbot response to the chat_log.csv file
+            with open('chat_log.csv', 'a', newline='', encoding='utf-8') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow([user_input_str, response, timestamp])
+
+            if response.lower() in ['goodbye', 'bye', 'thank you']:
+                st.write("Thank you for chatting with me. Have a great day!")
+                st.stop()
+
+    # Conversation History Menu
+    elif choice == "Conversation History":
+        # Display the conversation history in a collapsible expander
+        st.header("Conversation History")
+        with open('chat_log.csv', 'r', encoding='utf-8') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            next(csv_reader)  # Skip the header row
+            for row in csv_reader:
+                st.text(f"User: {row[0]}")
+                st.text(f"Chatbot: {row[1]}")
+                st.text(f"Timestamp: {row[2]}")
+                st.markdown("---")
+
+    elif choice == "About":
+        st.write("This is a Customer Support Chatbot. It is designed to help users with various customer service inquiries.")
+        
+        st.subheader("Project Overview:")
+
+        st.write("""In this project, a chatbot was developed to handle common customer support inquiries. It utilizes NLP techniques and Logistic Regression for understanding customer queries and providing appropriate responses. The chatbot was implemented using Streamlit for a user-friendly interface.""")
+        
+        st.subheader("Features:")
+        st.write("""
+            - Responds to common customer service queries like account issues, product information, and order status.
+            - Provides users with business hours and contact information.
+            - Keeps track of conversation history.
+        """)
+
+        st.subheader("Technology Stack:")
+        st.write("""
+            - **NLP**: Used to understand customer queries.
+            - **Logistic Regression**: Used for intent classification.
+            - **Streamlit**: A Python framework for building the chatbot interface.
+        """)
+
+if __name__ == '__main__':
+    main()
